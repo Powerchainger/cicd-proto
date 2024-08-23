c251a3a743f995581f3c93f2c0edb53c847eafe3
siarhei kharlap <siarhei.kharlap@ventionteams.com>
2024-08-23 10:39:23 +0200

    add test1

--- SimpleApi/Controllers/WeatherForecastController.cs
+++ SimpleApi/Controllers/WeatherForecastController.cs
@@ -41,4 +41,16 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+    [HttpGet(Name = "GetTest1")]
+    public IEnumerable<WeatherForecast> GetTest1()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
 }
